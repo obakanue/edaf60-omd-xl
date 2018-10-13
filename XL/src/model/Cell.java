1bebb37e7ca3b5d9367fc7bcf93b6cc2894f458e
Viktor Joelsson <v-j-@hotmail.com>
2018-10-13 17:23:11 +0200

    Shell_ext_version

--- XL/src/model/Cell.java
+++ XL/src/model/Cell.java
@@ -1,4 +1,8 @@
 package model;
 
+import model.expr.Environment;
+
 public interface Cell {
+
+    public abstract double cellValue(Environment env);
 }
--- XL/src/model/Sheet.java
+++ XL/src/model/Sheet.java
@@ -4,7 +4,7 @@ import model.expr.Environment;
 
 import java.util.Map;
 import java.util.TreeMap;
-
+//Lyssna på hela modellen -> lyssna på sheets
 public class Sheet implements Environment {
     Map<String, Cell> cellMap;
 

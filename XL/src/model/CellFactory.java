0511f20f00997b7b3f50b539761db35e0612d8e0
elirud <eli.rud@hotmail.com>
2018-10-14 15:18:22 +0200

    Implements CellFactory and adds a new error in XLException

--- XL/src/model/CellFactory.java
+++ XL/src/model/CellFactory.java
@@ -1,4 +1,23 @@
 package model;
 
+import util.XLException;
+
+import java.io.IOException;
+
+import model.expr.Expr;
+import model.expr.ExprParser;
+
 public class CellFactory {
+    public Cell cell(Sheet sheet, String addr, String value) {
+        if (value.startsWith("#")) {
+            return new CommentCell(sheet, addr, value);
+        } else {
+            try {
+                Expr expr = new ExprParser().build(value);
+                return new ExprCell(sheet, addr, expr);
+            } catch (IOException e) {
+                throw XLException.CELLBUILD_ERROR;
+            }
+        }
+    }
 }
--- XL/src/util/XLException.java
+++ XL/src/util/XLException.java
@@ -4,6 +4,7 @@ public class XLException extends RuntimeException {
     //
     // Här måste diverse exceptions skrivas like so
     public static final XLException RECURSIVECELL_ERROR = new XLException("One slot cannot be used multiple times");
+    public static final XLException CELLBUILD_ERROR = new XLException("Failed to create cell");
     public XLException(String message) {
         super(message);
     }

25bf4c1e13c36462ec02a2745a384083703250f5
666046 <v-j-@hotmail.com>
2018-10-14 15:48:14 +0200

    ExprCell_update

--- XL/src/model/ExprCell.java
+++ XL/src/model/ExprCell.java
@@ -1,11 +1,13 @@
 package model;
+import expr.*;
 
 public class ExprCell implements Cell {
 
     private Expr expr;
 
-    public ExprCell (Expr expr){
+    public ExprCell (Sheet sheet, String address, Expr expr){
         this.expr = expr;
+        this.sheet = sheet;
     }
 
     public double cellValue(Environment env){

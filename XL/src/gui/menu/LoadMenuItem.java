ce946bb6e60d50c124b65f279ee00e6595e97e32
gottl <bte15mgo@student.lu.se>
2018-10-14 23:36:24 +0200

    LoadMenuItem control check

--- XL/src/gui/menu/LoadMenuItem.java
+++ XL/src/gui/menu/LoadMenuItem.java
@@ -2,14 +2,22 @@ package gui.menu;
 
 import gui.StatusLabel;
 import gui.XL;
+
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.TreeMap;
 import javax.swing.JFileChooser;
+import model.CellFactory;
+import model.Sheet;
+import model.Cell;
+
 
 
 
 class LoadMenuItem extends OpenMenuItem {
 
-    private CellFactory() loadCellFactory;
+    private CellFactory loadCellFactory;
     private Sheet sheet;
 
 
@@ -20,24 +28,23 @@ class LoadMenuItem extends OpenMenuItem {
     }
 
     protected void action(String path) throws FileNotFoundException {
-        try{
+        try {
             BufferedReader file = new BufferedReader(new FileReader(path));
-            TreeMap<String, Cell> loadSheet = new TreeMap<String, Cell>();
-            while (ready()){
-                String line = readLine();
+            TreeMap<String, Cell> loadSheet = new TreeMap<>();
+            while (file.ready()) {
+                String line = file.readLine();
                 int seperate = line.indexOf('=');
-                String address = line.substring(0,address);
-                String value = line.substring(address + 1);
-                loadSheet.put(address, loadCellFactory.cell(sheet, value));
+                String address = line.substring(0, seperate);
+                String value = line.substring(seperate + 1);
+                loadSheet.put(address, loadCellFactory.cell(sheet, address, value));
 
             }
 
-        }catch
-            catch (FileNotFoundException e) {
+        } catch (Exception e) {
             statusLabel.setText("Unable to load file " + e);
+        }
     }
-
-    protected int openDialog(JFileChooser fileChooser) {
+    protected int openDialog (JFileChooser fileChooser){
         return fileChooser.showOpenDialog(xl);
     }
 }

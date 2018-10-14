41e8ca8738b899745a8d8d78c6401871b4e366f5
666046 <v-j-@hotmail.com>
2018-10-14 22:56:29 +0200

    LoadMenuItem_up

--- XL/src/gui/menu/LoadMenuItem.java
+++ XL/src/gui/menu/LoadMenuItem.java
@@ -5,14 +5,36 @@ import gui.XL;
 import java.io.FileNotFoundException;
 import javax.swing.JFileChooser;
 
+
+
 class LoadMenuItem extends OpenMenuItem {
- 
+
+    private CellFactory() loadCellFactory;
+    private Sheet sheet;
+
+
     public LoadMenuItem(XL xl, StatusLabel statusLabel) {
         super(xl, statusLabel, "Load");
+        loadCellFactory = new CellFactory();
+        sheet = new Sheet();
     }
 
     protected void action(String path) throws FileNotFoundException {
-        // TODO
+        try{
+            BufferedReader file = new BufferedReader(new FileReader(path));
+            TreeMap<String, Cell> loadSheet = new TreeMap<String, Cell>();
+            while (ready()){
+                String line = readLine();
+                int seperate = string.indexOf('=');
+                String address = line.substring(0,address);
+                String value = string.substring(address + 1);
+                loadSheet.put(address, loadCellFactory.cell(sheet, value));
+                sheet = loadSheet;
+            }
+
+        }catch
+            catch (FileNotFoundException e) {
+            statusLabel.setText("Unable to load file " + e);
     }
 
     protected int openDialog(JFileChooser fileChooser) {

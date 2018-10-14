52723ceeb6fdd906b7a36e260fde82b638c7b54a
elirud <eli.rud@hotmail.com>
2018-10-14 17:21:00 +0200

    Makes Editor implement Observer

--- XL/src/gui/Editor.java
+++ XL/src/gui/Editor.java
@@ -1,10 +1,17 @@
 package gui;
 
 import java.awt.Color;
+import java.util.Observable;
+import java.util.Observer;
 import javax.swing.JTextField;
 
-public class Editor extends JTextField {
+public class Editor extends JTextField implements Observer {
     public Editor() {
         setBackground(Color.WHITE);
+    }
+
+    @Override
+    public void update(Observable observable, Object object) {
+        
     }
 }

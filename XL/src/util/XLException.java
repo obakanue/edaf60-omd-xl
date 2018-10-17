b58312fc99538ab5acd1d4bf4f3e2ace0e79a8d4
Gottlander <bte15mgo@student.lu.se>
2018-10-17 19:27:17 +0200

    removed unneccesary code

--- XL/src/util/XLException.java
+++ XL/src/util/XLException.java
@@ -4,7 +4,6 @@ public class XLException extends RuntimeException {
 
     public static final XLException RECURSIVECELL_ERROR = new XLException("One slot cannot be used multiple times");
     public static final XLException CELLBUILD_ERROR = new XLException("Failed to create cell");
-    public static final XLException Null = new XLException("Cell deleted");
 
     public XLException(String message) {
         super(message);
